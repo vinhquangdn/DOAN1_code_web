860541b5aeb748970030dce0add6684d4d85ebc1
vinhquangdn <vinhquang2037@gmail.com>
2023-06-11 03:00:46 +0700

    code web

--- script.js
+++ script.js
@@ -19,33 +19,33 @@ if(snapshot.exists())
   {
       console.log("Receive data");
       firebase.database().ref("/He_thong_chong_trom/Phong_1").update({
-  "Btn_cb1": "0"});
+      "Btn_cb1": "0"});
   }
 else
   {
       console.log("No data available!");
       firebase.database().ref("/He_thong_chong_trom/Phong_1").update({
-  "Btn_cb1": "0"});
+      "Btn_cb1": "0"});
   }
 })
 
 var count_btn1 = 0;
 function func_sen1(){
   count_btn1 +=1;
-  alert(count_btn1);
+  //alert(count_btn1);
   if(count_btn1 %2 == 1)
       {
   
-  firebase.database().ref("/He_thong_chong_trom/Phong_1").update({
-   "Btn_cb1": "1" })
-  document.getElementById("ledsensor1_img").src = "./image/led1_on.png"
+        firebase.database().ref("/He_thong_chong_trom/Phong_1").update({
+        "Btn_cb1": "1" })
+        document.getElementById("ledsensor1_img").src = "./image/led1_on.png"
       }
   else
       {
   
-  firebase.database().ref("/He_thong_chong_trom/Phong_1").update({
-   "Btn_cb1": "0" })
-  document.getElementById("ledsensor1_img").src = "./image/led_off.png"
+        firebase.database().ref("/He_thong_chong_trom/Phong_1").update({
+        "Btn_cb1": "0" })
+        document.getElementById("ledsensor1_img").src = "./image/led_off.png"
       }
 }
   document.getElementById("btnsen1_js").addEventListener("click",func_sen1);
@@ -57,12 +57,16 @@ if (btnsen1_status == "1")
   {
       if(count_btn1 %2 == 0)
       {
-count_btn1 +=1;
+        count_btn1 +=1;
       }
       document.getElementById("ledsensor1_img").src = "./image/led1_on.png";
   }  
-else
+else if(btnsen1_status == "0")
   {
+      if(count_btn1 %2 == 1)
+      {
+        count_btn1 -=1;
+      }
       document.getElementById("ledsensor1_img").src = "./image/led_off.png";
   }
 });
@@ -73,33 +77,33 @@ if(snapshot.exists())
   {
       console.log("Receive data");
       firebase.database().ref("/He_thong_chong_trom/Phong_2").update({
-  "Btn_cb2": "0"});
+      "Btn_cb2": "0"});
   }
 else
   {
       console.log("No data available!");
       firebase.database().ref("/He_thong_chong_trom/Phong_2").update({
-  "Btn_cb2": "0"});
+      "Btn_cb2": "0"});
   }
 })
 
 var count_btn2 = 0;
 function func_sen2(){
   count_btn2 +=1;
-  alert(count_btn2);
+  //alert(count_btn2);
   if(count_btn2 %2 == 1)
       {
-  
-  firebase.database().ref("/He_thong_chong_trom/Phong_2").update({
-   "Btn_cb2": "1" })
-  document.getElementById("ledsensor2_img").src = "./image/led2_on.png"
+        
+        firebase.database().ref("/He_thong_chong_trom/Phong_2").update({
+        "Btn_cb2": "1" })
+        document.getElementById("ledsensor2_img").src = "./image/led2_on.png"
       }
   else
       {
-  
-  firebase.database().ref("/He_thong_chong_trom/Phong_2").update({
-   "Btn_cb2": "0" })
-  document.getElementById("ledsensor2_img").src = "./image/led_off.png"
+        
+        firebase.database().ref("/He_thong_chong_trom/Phong_2").update({
+        "Btn_cb2": "0" })
+        document.getElementById("ledsensor2_img").src = "./image/led_off.png"
       }
 }
   document.getElementById("btnsen2_js").addEventListener("click",func_sen2);
@@ -111,12 +115,16 @@ if (btnsen2_status == "1")
   {
       if(count_btn2 %2 == 0)
       {
-count_btn2 +=1;
+        count_btn2 +=1;
       }
       document.getElementById("ledsensor2_img").src = "./image/led2_on.png";
   }  
 else
   {
+      if(count_btn2 %2 == 1)
+      {
+        count_btn2 -=1;
+      }
       document.getElementById("ledsensor2_img").src = "./image/led_off.png";
   }
 });
@@ -126,33 +134,33 @@ if(snapshot.exists())
   {
       console.log("Receive data");
       firebase.database().ref("/He_thong_chong_trom/Thiet_bi_bao_trom").update({
-  "Btn_tbbt": "0"});
+      "Btn_tbbt": "0"});
   }
 else
   {
       console.log("No data available!");
       firebase.database().ref("/He_thong_chong_trom/Thiet_bi_bao_trom").update({
-  "Btn_tbbt": "0"});
+      "Btn_tbbt": "0"});
   }
 })
 
 var count_btnchuong = 0;
 function func_chuong(){
   count_btnchuong +=1;
-  alert(count_btnchuong);
+  //alert(count_btnchuong);
   if(count_btnchuong %2 == 1)
       {
   
-  firebase.database().ref("/He_thong_chong_trom/Thiet_bi_bao_trom").update({
-   "Btn_tbbt": "1" })
-  document.getElementById("ledchuong_img").src = "./image/ledchuong_on.png"
+        firebase.database().ref("/He_thong_chong_trom/Thiet_bi_bao_trom").update({
+        "Btn_tbbt": "1" })
+        document.getElementById("ledchuong_img").src = "./image/ledchuong_on.png"
       }
   else
       {
   
-  firebase.database().ref("/He_thong_chong_trom/Thiet_bi_bao_trom").update({
-   "Btn_tbbt": "0" })
-  document.getElementById("ledchuong_img").src = "./image/led_off.png"
+        firebase.database().ref("/He_thong_chong_trom/Thiet_bi_bao_trom").update({
+        "Btn_tbbt": "0" })
+        document.getElementById("ledchuong_img").src = "./image/led_off.png"
       }
 }
   document.getElementById("btnchuong_js").addEventListener("click",func_chuong);
@@ -164,16 +172,35 @@ if (btnchuong_status == "1")
   {
       if(count_btnchuong %2 == 0)
       {
-count_btnchuong +=1;
+          count_btnchuong +=1;
       }
       document.getElementById("ledchuong_img").src = "./image/ledchuong_on.png";
   }  
 else
   {
+      if(count_btnchuong %2 == 1)
+      {
+          count_btnchuong -=1;
+      }
       document.getElementById("ledchuong_img").src = "./image/led_off.png";
   }
 });
 //---------------------chuong, led canh bao------------------------------------------------
+
+firebase.database().ref("/He_thong_chong_trom/Thiet_bi_bao_trom/Chuong_canh_bao").get().then((snapshot) => {
+if(snapshot.exists())
+  {
+      console.log("Receive data");
+      firebase.database().ref("/He_thong_chong_trom/Thiet_bi_bao_trom").update({
+      "Chuong_canh_bao": "0"});
+  }
+else
+  {
+      console.log("No data available!");
+      firebase.database().ref("/He_thong_chong_trom/Thiet_bi_bao_trom").update({
+      "Chuong_canh_bao": "0"});
+  }
+})
 
 firebase.database().ref("/He_thong_chong_trom/Thiet_bi_bao_trom/Chuong_canh_bao").on("value",function(snapshot){
 var chuong_status = snapshot.val()
@@ -186,6 +213,21 @@ else
       document.getElementById("alarm_img").src = "./image/alarm_off.png";
   }
 });
+
+firebase.database().ref("/He_thong_chong_trom/Thiet_bi_bao_trom/Led_canh_bao").get().then((snapshot) => {
+if(snapshot.exists())
+  {
+      console.log("Receive data");
+      firebase.database().ref("/He_thong_chong_trom/Thiet_bi_bao_trom").update({
+      "Led_canh_bao": "0"});
+  }
+else
+  {
+      console.log("No data available!");
+      firebase.database().ref("/He_thong_chong_trom/Thiet_bi_bao_trom").update({
+      "Led_canh_bao": "0"});
+  }
+})
 
 firebase.database().ref("/He_thong_chong_trom/Thiet_bi_bao_trom/Led_canh_bao").on("value",function(snapshot){
 var ledwarn_status = snapshot.val()
